a4a94777b3bc8b07cb5d475ccf69e69818c1e3f4
Marcos808 <marcosfelipesantos182004@gmail.com>
2024-10-12 12:06:51 -0300

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,7 +13,7 @@ namespace TodoList
 
         void InitializePickers()
         {
-            // Distância
+         
             distanceFromPicker.Items.Add("Metros");
             distanceFromPicker.Items.Add("Quilômetros");
             distanceFromPicker.Items.Add("Milhas");
@@ -21,7 +21,7 @@ namespace TodoList
             distanceToPicker.Items.Add("Quilômetros");
             distanceToPicker.Items.Add("Milhas");
 
-            // Peso
+          
             weightFromPicker.Items.Add("Gramas");
             weightFromPicker.Items.Add("Quilogramas");
             weightFromPicker.Items.Add("Libras");
@@ -29,7 +29,7 @@ namespace TodoList
             weightToPicker.Items.Add("Quilogramas");
             weightToPicker.Items.Add("Libras");
 
-            // Temperatura
+           
             temperatureFromPicker.Items.Add("Celsius");
             temperatureFromPicker.Items.Add("Fahrenheit");
             temperatureFromPicker.Items.Add("Kelvin");
@@ -69,11 +69,11 @@ namespace TodoList
         {
             if (fromUnit == toUnit) return value;
 
-            // Converte para metros primeiro
+        
             if (fromUnit == "Quilômetros") value *= 1000;
             if (fromUnit == "Milhas") value *= 1609.34;
 
-            // Converte de metros para a unidade desejada
+            
             if (toUnit == "Quilômetros") return value / 1000;
             if (toUnit == "Milhas") return value / 1609.34;
 
@@ -84,11 +84,11 @@ namespace TodoList
         {
             if (fromUnit == toUnit) return value;
 
-            // Converte para gramas primeiro
+        
             if (fromUnit == "Quilogramas") value *= 1000;
             if (fromUnit == "Libras") value *= 453.592;
 
-            // Converte de gramas para a unidade desejada
+         
             if (toUnit == "Quilogramas") return value / 1000;
             if (toUnit == "Libras") return value / 453.592;
 
@@ -99,11 +99,11 @@ namespace TodoList
         {
             if (fromUnit == toUnit) return value;
 
-            // Converte para Celsius primeiro
+   
             if (fromUnit == "Fahrenheit") value = (value - 32) * 5 / 9;
             if (fromUnit == "Kelvin") value -= 273.15;
 
-            // Converte de Celsius para a unidade desejada
+        
             if (toUnit == "Fahrenheit") return (value * 9 / 5) + 32;
             if (toUnit == "Kelvin") return value + 273.15;
 
